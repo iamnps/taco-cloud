2a345cac6ce3aee86de9eb2c00cd48225e45ca52
iamnps <niepeishen@sina.com>
2021-07-15 19:55:43 +0800

    test

--- src/main/java/com/nps/tacocloud/dao/impl/JdbcTacoImpl.java
+++ src/main/java/com/nps/tacocloud/dao/impl/JdbcTacoImpl.java
@@ -6,6 +6,8 @@ import com.nps.tacocloud.data.Order;
 import com.nps.tacocloud.data.Taco;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
